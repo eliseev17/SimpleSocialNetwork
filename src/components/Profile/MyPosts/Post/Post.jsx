2c3b82f0b41f93cc305ef2cd5804dc451f0b1f96
e_lis_eev <elis.ivan17@gmail.com>
2021-09-24 00:57:04 +0300

    added css modules

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={s.item}>
+            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt=""/>
+            {props.message}
+            <div>
+                <span>Like</span> {props.likesCount}
+            </div>
+        </div>
+    )
+}
+
+export default Post;
