c5d1c0f5cb9351ef4da4fab0f7d067926b7ad9c3
e_lis_eev <elis.ivan17@gmail.com>
2022-02-11 00:15:09 +0300

    withRouter, HeaderContainer and auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img
                 src='https://uploads-ssl.webflow.com/5f8f7f6f45a2ba8926f2e652/5f9acb9b4c0d417a29a8dfbe_Exemplifi_Branding.png'
                 alt=''/>
+            <div className={s.loginBlock}>
+                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+            </div>
         </header>
     )
 }
